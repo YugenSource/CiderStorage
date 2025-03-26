6cef7731f2dddfce1f59c24dc6094b38cfd58196
silene0259 <195784300+YugenSource@users.noreply.github.com>
2025-03-26 17:31:26 -0400

    Add PoH

--- src/chain/consensus/poh.rs
+++ src/chain/consensus/poh.rs
@@ -1,1 +1,20 @@
-use poh_yugen::{PoHConfig, PoHUsage, PoHEntry};
+use poh_yugen::{PoHConfig, PoHUsage, PoHEntry, TickEntryType, InitialSeed};
+use sha2::Sha384;
+use sha2::Digest;
+use securerand_rs::rngs::FuschineCSPRNG;
+
+pub struct Consensus;
+
+impl Consensus {
+    pub fn new(init_data: Vec<u8>) {
+        // Initialize the PoH with a configuration
+        let config = PoHConfig::new(Sha384::new(), 48, 1000, 1000, false, true, TickEntryType::Empty);
+        let seed = FuschineCSPRNG::get_64_bytes_from_os();
+
+        
+        // Generate a random seed for the PoH
+        let poh = PoHUsage::new(config, seed, init_data, extensions)
+        // Example usage of the PoH instance
+        println!("PoH instance created: {:?}", poh_instance);
+    }
+}
